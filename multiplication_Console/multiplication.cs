be8fe898ac9ded27b7be283779ef0c3a143eb540
v7218812 <121347521+v7218812@users.noreply.github.com>
2022-12-25 23:26:40 +0800

    Add files via upload

---
+++ multiplication_Console/multiplication.cs
@@ -0,0 +1,21 @@
+﻿namespace multiplication_Console
+{
+    internal class multiplication
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                Console.WriteLine( );
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write(i + "*" + j + "=" + j * i);
+                    Console.Write("\n");
+                }
+
+
+            }
+
+        }
+    }
+}
